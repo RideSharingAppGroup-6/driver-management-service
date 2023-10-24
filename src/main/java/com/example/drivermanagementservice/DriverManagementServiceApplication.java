fbec0f851f74f308da4d4ca7b0772bff5def7e4e
Hema Bokam <hema17062000@gmail.com>
2023-10-24 10:15:58 +0530

    Intial setup and developed healthcheck api

---
+++ src/main/java/com/example/drivermanagementservice/DriverManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.drivermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/drivermanagementservice/DriverManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drivermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
